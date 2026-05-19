e3a42e348709a75a5355e6fcb4fdd1f28dbd8598
Apoorv Gupta <apoorvgpt9@gmail.com>
2026-05-19 11:42:11 +0000

    Referenced sources in staging models

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -7,35 +7,35 @@ with
         select * from {{ ref('stg_jaffle_shop__orders') }}
     ),
 
-    customer_orders as (
+    payments as (
+        select * from {{ ref('stg_stripe__payments') }}
+    ),
 
+    customer_orders as (
         select
             customer_id,
-
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
-
+            count(order_id) as number_of_orders,
+            sum(amount) as lifetime_value
         from orders
-
+        inner join payments using (order_id)
+        where payments.payment_status = 'success'
         group by 1
-
     ),
 
     final as (
-
         select
             customers.customer_id,
             customers.first_name,
             customers.last_name,
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            customer_orders.lifetime_value
+            
         from customers
-
         left join customer_orders using (customer_id)
-
     )
 
 select *
